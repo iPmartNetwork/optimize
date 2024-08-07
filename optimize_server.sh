841887355ece5b4e11d14faf6765524221d20a45
ali (zeus) <131793522+iPmartNetwork@users.noreply.github.com>
2024-08-07 21:04:46 +0330

    Update optimize_server.sh

--- optimize_server.sh
+++ optimize_server.sh
@@ -1,5 +1,39 @@
 #!/bin/bash
 
+# Color codes
+Purple='\033[0;35m'
+Cyan='\033[0;36m'
+cyan='\033[0;36m'
+CYAN='\033[0;36m'
+YELLOW='\033[0;33m'
+White='\033[0;96m'
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+NC='\033[0m' # No Color 
+
+# Check if the user has root access
+if [ "$EUID" -ne 0 ]; then
+  echo $'\e[32mPlease run with root privileges.\e[0m'
+  exit
+fi
+
+
+    echo -e "${Purple}"
+    cat << "EOF"
+          
+                 
+══════════════════════════════════════════════════════════════════════════════════════
+        ____                             _     _                                     
+    ,   /    )                           /|   /                                  /   
+-------/____/---_--_----__---)__--_/_---/-| -/-----__--_/_-----------__---)__---/-__-
+  /   /        / /  ) /   ) /   ) /    /  | /    /___) /   | /| /  /   ) /   ) /(    
+_/___/________/_/__/_(___(_/_____(_ __/___|/____(___ _(_ __|/_|/__(___/_/_____/___\__
+
+══════════════════════════════════════════════════════════════════════════════════════
+EOF
+    echo -e "${NC}"
+
 # Function to install necessary tools
 install_tools() {
     echo "Installing necessary tools..."
